9efe2a006a88a5d2e6919048df0c8a0e17291851
CarlosNoelJ <carlosnoelg3@outlook.com>
2019-09-19 23:27:53 -0500

    sign out button

--- src/app/core/side-bar/side-bar.component.ts
+++ src/app/core/side-bar/side-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BarService } from '../services/bar.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -7,8 +9,13 @@ import { BarService } from '../services/bar.service';
   styleUrls: ['./side-bar.component.scss']
 })
 export class SideBarComponent implements OnInit {
-  constructor(private barService: BarService) {}
+  constructor(private barService: BarService, private auth: AuthService, private router: Router) {}
 
   navItems$ = this.barService.options();
   ngOnInit() {}
+
+  signout() {
+    this.auth.logout();
+    this.router.navigateByUrl('/signin');
+  }
 }
